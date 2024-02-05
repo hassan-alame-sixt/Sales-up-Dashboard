c571428dd55d20e33e1c55fd6179b7911932b962
Hassan Alame <hassan.alame@sixt.com>
2024-02-05 11:44:16 +0100

    AAFES and First Sales Force

---
+++ Subscription Unlimited+ Revenue.sql
@@ -0,0 +1,144 @@
+--Accesss Management
+--Unlimited 
+
+
+"Unlimited +" as Product
+, Null as rsrv_resn
+, Null as rsrv_new_customer
+, Null as rsrv_status
+, Null as rsrv_status_extended
+, Null as rsrv_source_chl1
+, Null as rsrv_source_chl2
+, Null as rsrv_source_chl3
+, Null asrsrv_yield_source
+, Null as rsrv_yield_source_level2
+, Null as rsrv_yield_source_level3
+, Null as rsrv_posl_country_code
+, Null as rntl_mvnr
+, Null as rntl_mser
+, Null as rntl_konr
+, rnsb_aonr as rnsb_aonr
+, brnc_code_handover
+, brnc_name
+, brnc_operator
+, brnc_main_type
+, brnc_type_code
+, brnc_type
+, brnc_pool_code
+, brnc_pool_name
+, brnc_continent
+, brnc_country_code_iso
+, brnc_country
+, brnc_active_flg
+, brnc_corporate_franchise
+, brnc_country_region
+, brnc_country_region_franchise_breakdown
+, brnc_region
+, brnc_state
+, brnc_city
+, cleansed_dfr
+, cleansed_dfi
+, cstm_kdnr
+abkz
+cstm_name
+kdnr_prt_bed
+kdnr_bed_vkni
+kdnr_highest_person_region
+kdnr_highest_working_channel
+kdnr_prtn_person_region
+kdnr_prtn_working_channel
+kdnr_account_owner
+kdnr_highest_linked
+dto
+dto_name
+dfi
+dfi_name
+prtn_parent_domestic_country
+sf_account_owner_type
+sf_acquired_by_rent_id
+sf_acquired_rent_date
+sf_acquired_by_rent_region
+sf_inside_sales_person_region
+sf_acquired_by_rent_saleschannel
+sf_deal_amount
+sf_total_sixt_potential
+sf_total_customer_spend
+agnc_age
+age_name
+age_prt_bed
+age_highest_person_region
+age_highest_working_channel
+age_prtn_person_region
+age_prtn_working_channel
+age_account_owner
+age_highest_linked
+dtt
+dtt_name
+dfr
+dfr_name
+rntl_type_code
+rntl_type
+rntl_payment_type
+internet_reservation_resn
+app_reservation_resn
+rsrv_cancelled_resn
+rsrv_noshow_resn
+internet_reservation_mvnr
+app_reservation_mvnr
+rntl_one_way_mvnr
+rntl_correction_mvnr
+domestic_country_mvnr
+rntl_bev_mvnr
+cstm_account_manager_num
+cstm_account_manager_name
+rsrv_date
+rntl_handover_datm
+rntl_return_datm
+rntl_accounting_date
+vhgr_crs
+vhcl_group
+vhcl_checked_out_group
+vhcl_type
+vhcl_category_level1
+vhcl_category_level2
+vhcl_category_level3
+vhcl_category_level4
+vhcl_owner_status
+vhat_elty
+rate_id
+rate_prl
+rate_bundle
+rate_type
+rate_designation
+rate_type_level1_gare
+rate_type_level2_glev
+rate_type_level3_aknm
+rate_type_level4_aktv
+AKTV
+rate_crm_type_gare_clv
+rate_gdat
+rate_vdat
+rate_next_gdat
+rate_validity_end
+product_level1_source
+product_level2_car_truck
+product_level2_cnb_vnt
+product_level3_name
+chra_pos
+chra_inty
+chra_chco
+chrg_name
+rsts_excitement_num
+rsts_recommendation_num
+rntl_revenue
+rntl_discount
+rental_days
+advanced_booking
+chra_value
+Time_and_Mileage
+improved_revenue
+Inc_tot
+internet_reservation_revenue
+app_reservation_revenue
+domestic_revenue
+non_domestic_revenue
